bbe5d463a2259d48f27a31208c4a1801446e5aec
shadowolf <shadowolfbusiness@gmail.com>
2018-08-18 01:28:13 +1000

    Reworked PlayerJoin event a bit, to detect Server Operators.

--- src/com/shadowolf/zander/Events/events.java
+++ src/com/shadowolf/zander/Events/events.java
@@ -3,20 +3,23 @@ package com.shadowolf.zander.Events;
 import com.connorlinfoot.actionbarapi.ActionBarAPI;
 import com.shadowolf.zander.zander;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 public class events implements Listener {
     zander plugin = zander.getPlugin(zander.class);
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event){
+    public void onJoin(org.bukkit.event.player.PlayerJoinEvent event){
         Player player = event.getPlayer();
 
         event.setJoinMessage("");
-        player.sendMessage(ChatColor.GOLD + player.getName() + " has joined the server!");
-        ActionBarAPI.sendActionBar(player, ChatColor.GOLD + player.getName() + " has joined the server!");
+        if(player.isOp()) {
+            event.setJoinMessage(ChatColor.GOLD + "Server Operator " + player.getName() + " has joined the server!");
+        }else {
+            event.setJoinMessage(ChatColor.YELLOW + player.getName() + " has joined the server!");
+        }
     }
 }
