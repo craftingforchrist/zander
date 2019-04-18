fde0efaf42a9e38648bd20ee9dcf278eb04cfb9a
shadowolf <shadowolfbusiness@gmail.com>
2019-04-18 22:40:55 +1000

    Update dbinit.sql

--- dbinit.sql
+++ dbinit.sql
@@ -3,9 +3,9 @@ use zander;
 CREATE USER 'zander'@'%' IDENTIFIED WITH mysql_native_password BY 'Passwordzander321';
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 FLUSH PRIVILEGES;
-GRANT SELECT ON zander.* TO crutech@'%';
-GRANT INSERT ON zander.* TO crutech@'%';
-GRANT UPDATE ON zander.* TO crutech@'%';
+GRANT SELECT ON zander.* TO zander@'%';
+GRANT INSERT ON zander.* TO zander@'%';
+GRANT UPDATE ON zander.* TO zander@'%';
 
 CREATE TABLE playerdata (
   uuid VARCHAR(36) not null primary key,
