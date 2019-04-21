43827612bb2546cb798b3bf51fd9c76c108df765
Jason Ho <jasoryeh@gmail.com>
2019-04-20 20:19:45 -0700

    Resolves #5 - Allows toggling other people's fly, and makes sure user does command from in-game

--- src/main/java/com/shadowolfyt/zander/commands/fly.java
+++ src/main/java/com/shadowolfyt/zander/commands/fly.java
@@ -1,5 +1,6 @@
 package com.shadowolfyt.zander.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,23 +9,38 @@ import org.bukkit.entity.Player;
 
 public class fly implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player player = (Player) sender;
-
-        if (player.hasPermission("zander.fly")) {
-            if (player.isFlying() || player.getAllowFlight()) {
-                player.setAllowFlight(false);
-                player.setFlying(false);
-                player.sendMessage(ChatColor.GREEN + "You are no longer " + ChatColor.GOLD + "flying.");
-                return true;
+        if (sender.hasPermission("zander.fly")) {
+            if(args.length == 0) {
+                if(sender instanceof Player) {
+                    Player player = (Player) sender;
+                    toggleUserFly(player);
+                } else {
+                    sender.sendMessage("This command only works for you if you are in-game.");
+                }
             } else {
-                player.setAllowFlight(true);
-                player.setFlying(true);
-                player.sendMessage(ChatColor.GREEN + "You are now " + ChatColor.GOLD + "flying.");
-                return true;
+                Player target = Bukkit.getPlayer(args[0]);
+                if(target != null) {
+                    toggleUserFly(target);
+                    sender.sendMessage(ChatColor.GREEN + target.getName() + "'s fly was toggled.");
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Player not found.");
+                }
             }
         } else {
-            player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
-            return true;
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
         }
+        return true;
+    }
+
+    private boolean toggleUserFly(Player player) {
+        if(player == null) return false;
+
+        boolean fly = player.isFlying() || player.getAllowFlight();
+
+        player.setAllowFlight(!fly);
+        player.setFlying(!fly);
+        player.sendMessage(ChatColor.GREEN + "You are " + (!fly ? "no longer" : "now") + " flying.");
+
+        return true;
     }
 }
