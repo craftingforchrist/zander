675db83ab03126c644491e664984a92586a01162
AFluffyPlatypus <24725291+AFluffyPlatypus@users.noreply.github.com>
2019-05-29 21:34:56 +1000

    Update profile.java
    Hopefully this fixes that overlooked functionality. Also, I think you may have been misusing args[0], so I fixed that up too.

--- src/main/java/com/shadowolfyt/zander/discord/profile.java
+++ src/main/java/com/shadowolfyt/zander/discord/profile.java
@@ -31,8 +31,19 @@ public class profile extends ListenerAdapter {
             //
             try {
                 PreparedStatement findstatement = plugin.getConnection().prepareStatement("SELECT * FROM playerdata WHERE username=?");
-                findstatement.setString(1, args[0]);
+                // NOTE: I changed args[0] here to args[1], because args[0] would refer to "[PREFIX]profile", not the username requested.
+                findstatement.setString(1, args[1]);
                 ResultSet results = findstatement.executeQuery();
+                // If there are no such players.
+                // NOTE: I'm not sure whether line 39 will work, because I don't know exactly what the query will return if there's nothing, and have no way to test and find out.
+                if (results == null) {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setTitle("Error");
+                    embed.setColor(Color.red);
+                    embed.setDescription("The player " + args[1] + " does not exist on this server!");
+                    event.getChannel().sendMessage(embed.build()).queue();
+                }
+                // If there is exactly one such result, indicating that this is the player requested.
                 if (!results.next()) {
                     EmbedBuilder embed = new EmbedBuilder();
                     embed.setTitle(results.getString("username") + "'s Profile");
@@ -42,6 +53,7 @@ public class profile extends ListenerAdapter {
                     embed.addField("Last Seen", results.getString("lastseen"), true);
                     event.getChannel().sendMessage(embed.build()).queue();
                 }
+            // If there is a SQL exception.
             } catch (SQLException e) {
                 e.printStackTrace();
                 EmbedBuilder embed = new EmbedBuilder();
