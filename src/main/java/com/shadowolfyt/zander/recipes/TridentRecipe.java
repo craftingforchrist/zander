6be0f147dbff1d2f37352cc1361727a73e0c06e3
shadowolfyt <shadowolfbusiness@gmail.com>
2019-06-18 01:08:54 +1000

    Adding a custom Trident and Rotten Flesh to Leather recipe.

---
+++ src/main/java/com/shadowolfyt/zander/recipes/TridentRecipe.java
@@ -0,0 +1,27 @@
+package com.shadowolfyt.zander.recipes;
+
+import com.shadowolfyt.zander.ZanderMain;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+
+public class TridentRecipe implements Listener {
+    ZanderMain plugin;
+    public TridentRecipe(ZanderMain instance) {
+        plugin = instance;
+    }
+
+    public void TridentRecipe() {
+        ItemStack item = new ItemStack(Material.TRIDENT, 1);
+        NamespacedKey key = new NamespacedKey(plugin, "Trident");
+        ShapedRecipe tr = new ShapedRecipe(key, item);
+
+        tr.shape("$$$","@%@","@%@");
+        tr.setIngredient('$', Material.DIAMOND_BLOCK);
+        tr.setIngredient('@', Material.AIR);
+        tr.setIngredient('%', Material.CONDUIT);
+        plugin.getServer().addRecipe(tr);
+    }
+}
