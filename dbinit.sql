702e26e0397dcb089952574a5c0bddefbb6d879f
shadowolfyt <shadowolfbusiness@gmail.com>
2019-06-18 19:42:57 +1000

    Fixed and updated dbinit script.

--- dbinit.sql
+++ dbinit.sql
@@ -1,44 +1,52 @@
-create database zander;
-use zander;
+DROP DATABASE IF EXISTS zander;
+CREATE DATABASE IF NOT EXISTS zander;
+USE zander;
+
 CREATE USER 'zander'@'%' IDENTIFIED WITH mysql_native_password BY 'Passwordzander321';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 FLUSH PRIVILEGES;
 GRANT SELECT ON zander.* TO zander@'%';
 GRANT INSERT ON zander.* TO zander@'%';
 GRANT UPDATE ON zander.* TO zander@'%';
 
 CREATE TABLE playerdata (
-  uuid VARCHAR(36) not null primary key,
-  username TEXT,
-  joins int,
-  deaths int,
-  status TEXT,
-  lastseen TEXT,
-  totalplaytime TEXT,
-  isVerified TEXT,
+  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  uuid VARCHAR(36),
+  username VARCHAR(16),
+  joins int NOT NULL DEFAULT 0,
+  deaths int NOT NULL DEFAULT 0,
+  status ENUM('Currently Online', 'Offline'),
+  lastseen TIMESTAMP,
+  initaljoin TIMESTAMP,
+  totalplaytime BIGINT,
+  isVerified BOOLEAN DEFAULT false,
   discordid VARCHAR(18),
-  discorddiscriminator TEXT,
-  ipaddress TEXT,
-  bedlocation TEXT,
-  mobdeath_player int not null default 0,
-  mobdeath_creeper int not null default 0,
-  mobdeath_enderdragon int not null default 0,
-  mobdeath_wither int not null default 0,
-  mobdeath_elderguardian int not null default 0,
-  mobdeath_witherskeleton int not null default 0,
-  mobdeath_shulker int not null default 0,
-  mobdeath_evoker int not null default 0,
-  mobdeath_bat int not null default 0
+  discorddiscriminator VARCHAR(35),
+  ipaddress VARCHAR(45),
+  bedlocation TEXT
+ );
+
+ CREATE TABLE mobdeath (
+  player_id int NOT NULL DEFAULT 0,
+  mobdeath_player int NOT NULL DEFAULT 0,
+  mobdeath_creeper int NOT NULL DEFAULT 0,
+  mobdeath_enderdragon int NOT NULL DEFAULT 0,
+  mobdeath_wither int NOT NULL DEFAULT 0,
+  mobdeath_elderguardian int NOT NULL DEFAULT 0,
+  mobdeath_witherskeleton int NOT NULL DEFAULT 0,
+  mobdeath_shulker int NOT NULL DEFAULT 0,
+  mobdeath_evoker int NOT NULL DEFAULT 0,
+  mobdeath_bat int NOT NULL DEFAULT 0,
+  FOREIGN KEY (player_id) REFERENCES playerdata (id)
 );
 
 CREATE TABLE punishments (
-  id int AUTO_INCREMENT not null primary key,
-  punisheduseruuid VARCHAR(36),
-  punishedusername TEXT,
-  punisheruuid VARCHAR(36),
-  punisherusername TEXT,
-  punishplatform TEXT,
-  punishtype TEXT,
+  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  punisheduser_id int NOT NULL DEFAULT 0,
+  punisher_id int NOT NULL DEFAULT 0,
+  punishplatform ENUM('SERVER', 'DISCORD'),
+  punishtype ENUM('KICK', 'BAN', 'TEMP BAN'),
   reason TEXT,
-  punishtimestamp TEXT
+  punishtimestamp TIMESTAMP,
+  FOREIGN KEY (punisheduser_id) REFERENCES playerdata (id),
+  FOREIGN KEY (punisher_id) REFERENCES playerdata (id)
 );
