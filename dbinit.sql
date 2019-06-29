7056237ab2c27441b4fb527003562c3d6071865f
Ben Robson <shadowolfbusiness@gmail.com>
2019-06-29 23:45:08 +1000

    Update dbinit.sql

--- dbinit.sql
+++ dbinit.sql
@@ -12,41 +12,57 @@ CREATE TABLE playerdata (
   id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
   uuid VARCHAR(36),
   username VARCHAR(16),
-  joins int NOT NULL DEFAULT 0,
-  deaths int NOT NULL DEFAULT 0,
-  status ENUM('Currently Online', 'Offline'),
-  lastseen TIMESTAMP,
-  initaljoin TIMESTAMP,
-  totalplaytime BIGINT,
+  joined TIMESTAMP NOT NULL DEFAULT NOW(),
   isVerified BOOLEAN DEFAULT false,
   discordid VARCHAR(18),
   discorddiscriminator VARCHAR(35),
-  ipaddress VARCHAR(45),
-  bedlocation TEXT
+  bedlocation   POINT NULL
  );
+create index playerdata_username on playerdata (username);
 
- CREATE TABLE mobdeath (
-  player_id int NOT NULL DEFAULT 0,
-  mobdeath_player int NOT NULL DEFAULT 0,
-  mobdeath_creeper int NOT NULL DEFAULT 0,
-  mobdeath_enderdragon int NOT NULL DEFAULT 0,
-  mobdeath_wither int NOT NULL DEFAULT 0,
-  mobdeath_elderguardian int NOT NULL DEFAULT 0,
-  mobdeath_witherskeleton int NOT NULL DEFAULT 0,
-  mobdeath_shulker int NOT NULL DEFAULT 0,
-  mobdeath_evoker int NOT NULL DEFAULT 0,
-  mobdeath_bat int NOT NULL DEFAULT 0,
+
+ CREATE TABLE sessions (
+  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  player_id INT NOT NULL DEFAULT 0,
+  sessionstart TIMESTAMP NOT NULL DEFAULT NOW(),
+  sessionend   TIMESTAMP NULL,
+  ipaddress VARCHAR(45),
   FOREIGN KEY (player_id) REFERENCES playerdata (id)
 );
+create index sessions_player_id    on sessions (player_id);
+create index sessions_sessionstart on sessions (sessionstart);
+create index sessions_sessionend   on sessions (sessionend);
+
+
+ CREATE TABLE mobtypes (
+  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  displayorder INT NULL,
+  mobtype VARCHAR(32) NOT NULL
+);
+create index mobtypes_mobtype on mobtypes (mobtype);
+insert into mobtypes (displayorder, mobtype) values (1, 'Player'), (2, 'Creeper'), (3, 'Ender Dragon'), (4, 'Wither'), (5, 'Elder Guardian'), (6, 'Wither Skeleton'), (7, 'Shulker'), (8, 'Evoker'), (9, 'Bat');
+
+
+ CREATE TABLE mobdeath (
+  death_time TIMESTAMP NOT NULL DEFAULT NOW(),
+  killer_id INT NULL,
+  mobtype_id INT NOT NULL DEFAULT 0,
+  killed_id INT NULL,
+  location POINT NULL,
+  FOREIGN KEY (killer_id) REFERENCES playerdata (id),
+  FOREIGN KEY (killed_id) REFERENCES playerdata (id),
+  FOREIGN KEY (mobtype_id) REFERENCES mobtypes (id)
+);
+
 
 CREATE TABLE punishments (
   id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  punisheduser_id int NOT NULL DEFAULT 0,
-  punisher_id int NOT NULL DEFAULT 0,
+  punisheduser_id INT NOT NULL DEFAULT 0,
+  punisher_id INT NOT NULL DEFAULT 0,
   punishplatform ENUM('SERVER', 'DISCORD'),
   punishtype ENUM('KICK', 'BAN', 'TEMP BAN'),
   reason TEXT,
-  punishtimestamp TIMESTAMP,
+  punishtimestamp TIMESTAMP NOT NULL DEFAULT NOW(),
   FOREIGN KEY (punisheduser_id) REFERENCES playerdata (id),
   FOREIGN KEY (punisher_id) REFERENCES playerdata (id)
 );
