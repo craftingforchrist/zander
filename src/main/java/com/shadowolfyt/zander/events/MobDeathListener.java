1e46b00802a0214985f0ea4a588cc4dfd712db1f
benrobson8 <csceg12188>
2019-07-03 01:20:06 +1000

    If server unexpectedly crashes, on reboot, set sessions to the current boot time.

--- src/main/java/com/shadowolfyt/zander/events/MobDeathListener.java
+++ src/main/java/com/shadowolfyt/zander/events/MobDeathListener.java
@@ -1,6 +1,7 @@
 package com.shadowolfyt.zander.events;
 
 import com.shadowolfyt.zander.ZanderMain;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -25,78 +26,19 @@ public class MobDeathListener implements Listener {
     public void onEntityDeath(EntityDeathEvent event) {
         if (event.getEntity().getKiller() == null) return;
 
-        if (event.getEntityType() == EntityType.PLAYER) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_player = mobdeath_player+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.CREEPER) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_creeper = mobdeath_creeper+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.ENDER_DRAGON) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_enderdragon = mobdeath_enderdragon+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.WITHER) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_wither = mobdeath_wither+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.ELDER_GUARDIAN) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_elderguardian = mobdeath_elderguardian+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.WITHER_SKELETON) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_witherskeleton = mobdeath_witherskeleton+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.SHULKER) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_shulker = mobdeath_shulker+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.EVOKER) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_evoker = mobdeath_evoker+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (event.getEntityType() == EntityType.BAT) {
-            try {
-                PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE playerdata SET mobdeath_bat = mobdeath_bat+1 WHERE uuid=?");
-                updatestatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
-                updatestatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else return;
+        //
+        // Database Query
+        // Log a mob death.
+        //
+        try {
+            PreparedStatement insertstatement = plugin.getConnection().prepareStatement("INSERT INTO mobdeath (killer_id, mobtype_id, location) VALUES ((select id from playerdata where uuid = ?), (select id from mobtypes where mobtype = ?), POINT(?, ?))");
+            insertstatement.setString(1, event.getEntity().getKiller().getUniqueId().toString());
+            insertstatement.setString(2, event.getEntityType().name());
+            insertstatement.setFloat(3, event.getEntity().getLocation().getBlockX());
+            insertstatement.setFloat(4, event.getEntity().getLocation().getBlockZ());
+            insertstatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
