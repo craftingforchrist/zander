b8d2797be291b4aab72ba81550fbac8fb926c9cb
benrobson8 <csceg12188>
2019-08-20 23:36:06 +1000

    Started integrating web users into database.

--- dbinit.sql
+++ dbinit.sql
@@ -15,7 +15,7 @@ CREATE TABLE playerdata (
   joined TIMESTAMP NOT NULL DEFAULT NOW(),
   deaths int DEFAULT 0,
   bedlocation POINT NULL
- );
+);
 create index playerdata_username on playerdata (username);
 INSERT INTO playerdata (uuid, username) VALUES ('f78a4d8d-d51b-4b39-98a3-230f2de0c670', 'CONSOLE');
 
@@ -23,7 +23,7 @@ CREATE TABLE sessions (
   id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
   player_id INT NOT NULL DEFAULT 0,
   sessionstart TIMESTAMP NOT NULL DEFAULT NOW(),
-  sessionend   TIMESTAMP NULL,
+  sessionend TIMESTAMP NULL,
   ipaddress VARCHAR(45),
   FOREIGN KEY (player_id) REFERENCES playerdata (id)
 );
@@ -35,9 +35,17 @@ CREATE TABLE punishments (
   id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
   punisheduser_id INT NOT NULL DEFAULT 0,
   punisher_id INT NOT NULL DEFAULT 0,
-  punishtype ENUM('KICK', 'BAN', 'TEMP BAN'),
+  punishtype ENUM('KICK', 'BAN', 'TEMP BAN', 'MUTE'),
   reason TEXT,
   punishtimestamp TIMESTAMP NOT NULL DEFAULT NOW(),
   FOREIGN KEY (punisheduser_id) REFERENCES playerdata (id),
   FOREIGN KEY (punisher_id) REFERENCES playerdata (id)
+);
+
+CREATE TABLE webusers (
+  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  username VARCHAR(16),
+  email VARCHAR(150),
+  password BINARY(60),
+  registered TIMESTAMP NOT NULL DEFAULT NOW()
 );
