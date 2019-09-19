d3c2e71890b87dc9a763b050e342de8b394d8452
benrobson8 <csceg12188>
2019-09-19 14:05:46 +1000

    Closed #133. Fixed Crafting Disabled items returning null.

--- src/main/java/com/shadowolfyt/zander/events/CraftDisable.java
+++ src/main/java/com/shadowolfyt/zander/events/CraftDisable.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -18,9 +19,11 @@ public class CraftDisable implements Listener {
     }
 
     @EventHandler
-    public void CraftDisable(PrepareItemCraftEvent event) {
+    public void CraftDisable(CraftItemEvent event) {
         Material itemType = event.getRecipe().getResult().getType();
         Byte itemData = event.getRecipe().getResult().getData().getData();
+
+        if (event.getRecipe().getResult() == null) return;
 
         if (itemType == Material.END_CRYSTAL) {
             event.getInventory().setResult(new ItemStack(Material.AIR));
