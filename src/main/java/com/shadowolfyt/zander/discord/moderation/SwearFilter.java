ad35c9fc95a2e551953f3fb3bccef151431fe06c
Alvin Zhao <ayzee@hotmail.com>
2019-09-23 00:57:59 +1000

    Update SwearFilter.java #174
    Need to add a check for if message is a Foul Language Embed

--- src/main/java/com/shadowolfyt/zander/discord/moderation/SwearFilter.java
+++ src/main/java/com/shadowolfyt/zander/discord/moderation/SwearFilter.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.awt.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SwearFilter extends ListenerAdapter {
     private ZanderMain plugin;
@@ -19,18 +20,25 @@ public class SwearFilter extends ListenerAdapter {
         List<String> FilteredWords = plugin.configurationManager.getfilter().getStringList("filteredwords");
         String message = event.getMessage().getContentRaw();
 
+
+
+        if (event.getAuthor().isBot()) { //check if message was the foul language embed && event.getMessage().getContentDisplay().contains("Foul Language")
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                event.getMessage().delete().queue();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         for (String word: FilteredWords){
             if (message.toLowerCase().matches("^" + word + "$")){
-                if (event.getAuthor().isBot() && event.getMessage().getContentRaw().contains(word)){
-                    event.getMessage().delete().queue();
-                } else {
-                    event.getMessage().delete().queue();
-                    EmbedBuilder embed = new EmbedBuilder();
-                    embed.setTitle("Foul Language");
-                    embed.setColor(Color.red);
-                    embed.setDescription(event.getAuthor().getName() + " you cannot use that word!");
-                    event.getChannel().sendMessage(embed.build()).queue();
-                }
+                event.getMessage().delete().queue();
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setTitle("Foul Language");
+                embed.setColor(Color.red);
+                embed.setDescription(event.getAuthor().getName() + " you cannot use that word!");
+                event.getChannel().sendMessage(embed.build()).queue();
             }
         }
     }
