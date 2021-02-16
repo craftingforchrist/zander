6d79f68033b323ac83a1196052dc07ecc85535a3
benrobson <benrobson76@gmail.com>
2021-02-16 16:08:49 +1100

    Change PlayerOnServerConnect event to ServerSwitchEvent.

--- src/main/java/net/craftingforchrist/zander/events/PlayerOnServerConnect.java
+++ src/main/java/net/craftingforchrist/zander/events/PlayerOnServerConnect.java
@@ -7,6 +7,7 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -19,7 +20,7 @@ public class PlayerOnServerConnect implements Listener {
     private ZanderBungeeMain plugin = ZanderBungeeMain.getInstance();
 
     @EventHandler
-    public void PlayerServerSwitchEvent(ServerConnectedEvent event) {
+    public void PlayerServerSwitchEvent(ServerSwitchEvent event) {
         ProxiedPlayer player = event.getPlayer();
 
         //
@@ -28,16 +29,16 @@ public class PlayerOnServerConnect implements Listener {
         //
         try {
             PreparedStatement updatestatement = plugin.getConnection().prepareStatement("UPDATE gamesessions SET server = ? WHERE (select id from playerdata where uuid = ?) AND sessionend IS NULL");
-            updatestatement.setString(1, event.getServer().getInfo().getName());
+            updatestatement.setString(1, event.getFrom().getName());
             updatestatement.setString(2, event.getPlayer().getUniqueId().toString());
             updatestatement.executeUpdate();
-            plugin.getProxy().getConsole().sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', Variables.developmentprefix + " " + player.getDisplayName() + " switched to " + event.getServer().getInfo().getName() + ".")));
+            plugin.getProxy().getConsole().sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', Variables.developmentprefix + " " + player.getDisplayName() + " switched to " + event.getFrom().getName() + ".")));
 
             //
             // Discord Chat Logs
             //
             TextChannel textChannel = jda.getTextChannelsByName(plugin.configurationManager.getConfig().getString("discord.chatlogchannel"), true).get(0);
-            textChannel.sendMessage("** :twisted_rightwards_arrows: ** | " + player.getDisplayName() + " switched to " + event.getServer().getInfo().getName() + ".").queue();
+            textChannel.sendMessage("** :twisted_rightwards_arrows: ** | " + player.getDisplayName() + " switched to " + event.getFrom().getName() + ".").queue();
 
         } catch (SQLException e) {
             e.printStackTrace();
