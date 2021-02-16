748655095a826a7e46ab000037a0c9ee208e9bf3
benrobson <benrobson76@gmail.com>
2021-02-16 16:15:43 +1100

    Change PlayerOnJoin event to PostLoginEvent.

--- src/main/java/net/craftingforchrist/zander/events/PlayerOnJoin.java
+++ src/main/java/net/craftingforchrist/zander/events/PlayerOnJoin.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -20,7 +21,7 @@ public class PlayerOnJoin implements Listener {
     private ZanderBungeeMain plugin = ZanderBungeeMain.getInstance();
 
     @EventHandler
-    public void PlayerOnJoin(ServerConnectedEvent event) {
+    public void PlayerOnJoin(PostLoginEvent event) {
         ProxiedPlayer player = event.getPlayer();
         plugin.getLogger().info(player.getDisplayName() + " has joined the server");
 
